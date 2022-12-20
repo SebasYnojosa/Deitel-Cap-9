06a29c778f5c7ba21587f9ec486b4c308fefc584
Sebastian Ynojosa <emilio240104@gmail.com>
2022-12-20 19:33:31 -0400

    Mas casos Figura 2

--- 9.2_flotantes.c
+++ 9.2_flotantes.c
@@ -10,6 +10,11 @@ int main(){
     printf("%f\n", 1234567.89);
     printf("%g\n", 1234567.89);
     printf("%G\n", 1234567.89);
+    printf("%e\n", 0.0000875);
+    printf("%E\n", 0.0000875);
+    printf("%f\n", 0.0000875);
+    printf("%g\n", 0.0000875);
+    printf("%G\n", 0.0000875);
 
     return 0;
 }
