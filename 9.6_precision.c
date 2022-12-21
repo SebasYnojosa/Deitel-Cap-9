c4a8392c9cf4232af9d4c468b5e179604629dec0
Sebastian Ynojosa <emilio240104@gmail.com>
2022-12-21 13:58:52 -0400

    Figura 6

---
+++ 9.6_precision.c
@@ -0,0 +1,18 @@
+/* Usando precision mientras se imprimen enteros, numeros flotantes y cadenas */
+
+#include <stdio.h>
+
+int main(){
+    int i = 873;
+    float f = 123.94536;
+    char s[] = "Feliz Cumple";
+
+    printf("Usando precision para enteros\n");
+    printf("\t%.4d\n\t%.9d\n\n", i, i);
+    printf("Usando precision para numeros flotantes\n");
+    printf("\t%.3f\n\t%.3e\n\t%.3g\n\n", f, f, f);
+    printf("Usando precision para cadenas\n");
+    printf("\t%.11s\n", s);
+
+    return 0;
+}
